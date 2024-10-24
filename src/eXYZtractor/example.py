16350531102de5bd43f75f15bbd04d61d54c6bae
StefanPSchmid <schmiste@ethz.ch>
2024-10-24 17:27:46 +0200

    add example pdfs

--- src/eXYZtractor/example.py
+++ src/eXYZtractor/example.py
@@ -1,5 +1,8 @@
 from eXYZtractor.extract_text_xyz import get_xyz_from_pdf
+import os
 
-path = 'example_text.pdf'
+folder = 'example_pdfs'
+path_list = [os.path.join(folder, file) for file in os.listdir(folder) if file.endswith('.pdf')]
 
-get_xyz_from_pdf(path=path)
+for i, path in enumerate(path_list):
+    get_xyz_from_pdf(path=path, prefix=f'example_{i}')
--- src/eXYZtractor/extract_text_xyz.py
+++ src/eXYZtractor/extract_text_xyz.py
@@ -20,6 +20,37 @@ def write_text_to_xyz(fname, text):
         for line in text:
             file.write(line if line.endswith("\n") else f"{line}\n")
 
+def determine_filename(relevant_lines, detected_text, num_xyzs, prefix):
+    """
+    This function determines the filename based on the last three preceding lines.
+    """
+    if prefix is not None:
+        return f'{prefix}_{num_xyzs}.xyz'
+    else:
+        xyz_length = len(detected_text)
+        for i in range(len(relevant_lines)-1, -1, -1):
+            print(relevant_lines[i])
+            line = relevant_lines[i].strip()
+            words = line.split()
+            skip_line = False
+            for word in words:
+                try:
+                    num_value = float(word)
+                    if num_value != int(num_value) or int(num_value) == xyz_length:
+                        skip_line = True
+                        break
+
+                except ValueError:
+                    continue
+        
+            if not skip_line:
+                line = line.replace(' ', '_').replace('.', '')
+                filename = f"{line}.xyz"
+                return filename
+    
+    return f'Coordinates_{num_xyzs}.xyz'
+
+
 def get_xyz_from_pdf(path, prefix=None):
 
     pattern_match = r'\b([A-Z][a-z]?|\d+)\s+([-+]?\d*\.?\d+)\s+([-+]?\d*\.?\d+)\s+([-+]?\d*\.?\d+)'
@@ -27,6 +58,7 @@ def get_xyz_from_pdf(path, prefix=None):
 
     text_ongoing = False
     detected_text = []
+    preceding_lines = []
     num_xyzs = 0
 
     for page_num in range(len(reader.pages)):
@@ -38,13 +70,23 @@ def get_xyz_from_pdf(path, prefix=None):
         for line in text:
             found, matching_part = check_and_extract_matching_part(line, pattern_match)
             if found:
+                if len(detected_text) == 0:  # Start of new XYZ block
+                    if len(preceding_lines) >= 3:
+                        comment_lines = preceding_lines[-3:]
+                    else:
+                        comment_lines = preceding_lines[:]
                 text_ongoing = True
                 detected_text.append(matching_part)
             else:
                 text_ongoing = False
 
+            if len(preceding_lines) >= 3:
+                preceding_lines.pop(0)
+            preceding_lines.append(line)
+
             if text_ongoing == False and len(detected_text) > 0:
-                write_text_to_xyz(fname=f'Coordinates_{num_xyzs}.xyz' if prefix is None else f'{prefix}_{num_xyzs}.xyz', text=detected_text)
+                output_fname = determine_filename(comment_lines, detected_text, num_xyzs, prefix)
+                write_text_to_xyz(fname=output_fname, text=detected_text)
                 detected_text = []
                 num_xyzs += 1
 
